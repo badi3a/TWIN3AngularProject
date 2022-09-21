36e75c43870b0b8cc673e0288641b999326dbe6e
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-09-21 12:04:21 +0200

    update home page with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
