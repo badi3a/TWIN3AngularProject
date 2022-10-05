6eb06c49c9a433818e44bb0d6bd0bed6711fd5dd
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-10-05 09:22:15 +0200

    session 4

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -1,3 +1,4 @@
+import { User } from './../model/user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent implements OnInit {
-
+  list: User[]
   constructor() { }
-
   ngOnInit(): void {
+    this.list=[
+      {
+        id: 1,
+        category: 'simple user',
+        name: 'James Nix',
+        job: 'Full Stack Developer',
+        phone: '046 5685 6969',
+        email:'JamesNix@spy.com',
+        address:'5 Boar Lane SELLING 2LG',
+        picture:'https://bootdey.com/img/Content/avatar/avatar2.png'
+      },
+      {
+        id: 2,
+        name: 'Darlene Smith',
+        category: 'admin',
+        job: 'UI/UX Designer',
+        phone: '012 6587 1236',
+        email:'DarleneSmith@spy.com',
+        address:'57 Guildry Street GAMRIE',
+        picture:'https://bootdey.com/img/Content/avatar/avatar3.png'
+      },
+      {
+        id: 2,
+        name: 'William Swift',
+        category: 'simple user',
+        job: 'Backend Developer',
+        phone: '012 6587 1236',
+        email:' WilliamSwift@spy.co',
+        address:'80 South Street MONKW 7BR',
+        picture:'https://bootdey.com/img/Content/avatar/avatar4.png'
+      },
+    ]
   }
+
 
 }
