2407b59fd9afff46aa52acbf545a1ff5ac6755f5
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-10-26 14:04:31 +0200

    update product serviceé

--- src/app/products/admin-product/admin-product.component.ts
+++ src/app/products/admin-product/admin-product.component.ts
@@ -8,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-product.component.css']
 })
 export class AdminProductComponent implements OnInit {
-  public list : Product[];
-  constructor(private productService:ProductService) { }
+  public list: Product[];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.list=this.productService.
-    listProduct.
-    filter(p => p.quantity==0);
+    this.list = this.productService.
+      listProduct.
+      filter(p => p.quantity == 0);
+  }
+
+  delete(p: Product) {
+    let i = this.list.indexOf(p);
+    this.list.splice(i, 1);
+    let index = this.productService.listProduct.indexOf(p);
+    this.productService.listProduct.splice(index, 1)
   }
 
 }
